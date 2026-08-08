31065fcf23da4e42e3ccab9a30b07e25e19a02e3
virginiasatyro <vivi_satyro@hotmail.com>
2026-08-08 18:33:37 -0300

    adding 2023 workouts

--- data/oldWorkouts2023.js
+++ data/oldWorkouts2023.js
@@ -1,1 +1,132 @@
-window.OLD_WORKOUTS_2023 = [];
+window.OLD_WORKOUTS_2023 = [
+  {
+    id: 202301,
+    year: 2023,
+    name: "Treino Novembro-Dezembro 2023",
+    active: false,
+    workouts: [
+      {
+        id: "1",
+        name: "Treino 1",
+        exercises: [
+          { id: 1, exerciseId: "banco-supino-reto", sets: 4, reps: "6", history: [{ date: "2023-11-01", weight: 7.5 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 2, exerciseId: "barra-fixa-pegada-fechada", sets: 4, reps: "6", history: [{ date: "2023-11-01", weight: 45 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 3, exerciseId: "desenvolvimento-barra-reta-atras", sets: 4, reps: "6", history: [{ date: "2023-11-01", weight: 7.5 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 4, exerciseId: "agachamento-hack", sets: 4, reps: "6", history: [{ date: "2023-11-01", weight: 20 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 5, exerciseId: "cadeira-flexora", sets: 4, reps: "6", history: [{ date: "2023-11-01", weight: 45 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 6, exerciseId: "abdominal-prancha-isometrica", sets: 4, reps: "60 segundos", history: [{ date: "2023-11-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 60 }
+        ]
+      },
+      {
+        id: "2",
+        name: "Treino 2",
+        exercises: [
+          { id: 1, exerciseId: "crucifixo-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 8 }] },
+          { id: 2, exerciseId: "flexao-bracos", sets: 3, reps: "10" },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 3, exerciseId: "banco-supino-inclinado", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 4 }] },
+          { id: 4, exerciseId: "elevacao-frontal-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 5 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 5, exerciseId: "puxada-aberta-barra-reta", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 38 }] },
+          { id: 6, exerciseId: "remada-curvada-halteres-pegada-neutra", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 12 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 7, exerciseId: "elevacao-lateral-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 6 }] },
+          { id: 8, exerciseId: "crucifixo-inverso-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 6 }] },
+          { type: "rest", name: "Descanso", duration: 60 }
+        ]
+      },
+      {
+        id: "3",
+        name: "Treino 3",
+        exercises: [
+          { id: 1, exerciseId: "triceps-frances-barra", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 2.5 }] },
+          { id: 2, exerciseId: "triceps-testa-barra-w", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 2.5 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 3, exerciseId: "rosca-direta-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 8 }] },
+          { id: 4, exerciseId: "rosca-direta-isometrica-halteres", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 5 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 5, exerciseId: "abducao-quadril-maquina", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 60 }] },
+          { id: 6, exerciseId: "afundo-alternado-salto", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 7, exerciseId: "abducao-quadril-maquina", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 50 }] },
+          { id: 8, exerciseId: "agachamento-com-salto", sets: 3, reps: "10", history: [{ date: "2023-11-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 60 }
+        ]
+      }
+    ]
+  },
+  {
+    id: 202302,
+    year: 2023,
+    name: "Treino Setembro-Outubro 2023",
+    active: false,
+    workouts: [
+      {
+        id: "1",
+        name: "Treino 1",
+        exercises: [
+          { id: 1, exerciseId: "banco-supino-reto", sets: 3, reps: "5", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 2, exerciseId: "remada-baixa-barra-h", sets: 3, reps: "5", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 3, exerciseId: "desenvolvimento-barra-reta-atras", sets: 3, reps: "5", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 4, exerciseId: "stiff-pes-proximos-barra-livre", sets: 3, reps: "5", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 90 },
+          { id: 5, exerciseId: "abdominal-prancha-isometrica", sets: 3, reps: "60 segundos", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 60 },
+          { id: 6, exerciseId: "abdominal-infra-banco-declinado-pernas-estendidas-elevacao-quadril", sets: 3, reps: "12", rest: 60 },
+          { type: "rest", name: "Descanso", duration: 60 }
+        ]
+      },
+      {
+        id: "2",
+        name: "Treino 2",
+        exercises: [
+          { id: 1, exerciseId: "supino-maquina-pegada-neutra", sets: 4, reps: "10" },
+          { id: 2, exerciseId: "flexao-bracos", sets: 4, reps: "10" },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 3, exerciseId: "triceps-coice-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 4, exerciseId: "triceps-frances-anilha", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 5, exerciseId: "elevacao-frontal-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 6, exerciseId: "afundo-pliometrico", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 7, exerciseId: "abducao-quadril-maquina", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 8, exerciseId: "agachamento-isometrico", sets: 4, reps: "60 segundos", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 9, exerciseId: "aducao-quadril-maquina", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 10, exerciseId: "agachamento-sumo-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 }
+        ]
+      },
+      {
+        id: "3",
+        name: "Treino 3",
+        exercises: [
+          { id: 1, exerciseId: "puxada-articulada-aberta", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 2, exerciseId: "crucifixo-inverso-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 3, exerciseId: "rosca-direta-barra-w", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 4, exerciseId: "rosca-direta-isometrica-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 5, exerciseId: "elevacao-lateral-halteres", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 6, exerciseId: "agachamento-com-salto", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 7, exerciseId: "cadeira-extensora", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 8, exerciseId: "abdominal-obliquo-alternado-pedalada", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 },
+          { id: 9, exerciseId: "cadeira-flexora", sets: 4, reps: "10", history: [{ date: "2023-09-01", weight: 0 }] },
+          { id: 10, exerciseId: "abdominal-remador", sets: 4, reps: "20", history: [{ date: "2023-09-01", weight: 0 }] },
+          { type: "rest", name: "Descanso", duration: 45 }
+        ]
+      }
+    ]
+  }
+];
